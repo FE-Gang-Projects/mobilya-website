1b64dad90750306d8c1a22f267d0c8963c9cd37f
Can Berk Sanal <sanalcanberk@gmail.com>
2022-05-13 11:22:02 +0300

    header ui done

--- frontend/components/Header/index.tsx
+++ frontend/components/Header/index.tsx
@@ -1,5 +1,57 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 const Header = () => {
-  return <header>xx</header>;
+  const headerTopLinks = [
+    { name: 'Tüm Ürünler', location: '/' },
+    { name: 'Hakkımızda', location: '/' },
+    { name: 'İletişim', location: '/' },
+  ];
+  const headerBottomLinks = [
+    { name: 'Düğün Paketi', location: '/' },
+    { name: 'Düğün Paketi', location: '/' },
+    { name: 'Oturma Grubu', location: '/' },
+    { name: 'Yemek Odası', location: '/' },
+    { name: 'Yatak Odası', location: '/' },
+    { name: 'Genç Odası', location: '/' },
+    { name: 'Genç Odası', location: '/' },
+  ];
+  return (
+    <header className="header">
+      <div className="header-top">
+        <Link href="/">
+          <Image src="/logos/logo.jpg" width={255.5} height={36} alt="Çalışkan Mobilya Logo" />
+        </Link>
+        <div className="header-top-options">
+          {headerTopLinks.map((option) => (
+            <Link key={option.name} href={option.location}>
+              {option.name}
+            </Link>
+          ))}
+        </div>
+        <div className="header-top-utils">
+          <div className="header-top-search-container">
+            <div className="search-icon-container">
+              <Image src="/icons/search.svg" width={21} height={20} alt="Arama İkonu" />
+            </div>
+            <input type="text" placeholder="Site içi ürün arama" />
+            <button>ARA</button>
+          </div>
+          <div className="header-top-favorites-container">
+            <Image src="/icons/favorites.svg" width={21} height={20} alt="Arama İkonu" />
+            <span>Favorilerim</span>
+          </div>
+        </div>
+      </div>
+      <div className="header-bottom">
+        {headerBottomLinks.map((option, i) => (
+          <Link key={i} href={option.location}>
+            {option.name}
+          </Link>
+        ))}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -7,21 +7,24 @@ import Image from 'next/image';
 const Home = () => {
   const img = useRef(null);
 
-  useEffect(() => {
-    const x = new hoverEffect({
-      parent: img.current,
-      intensity: 0.3,
-      image1: './images/slider-img1.jpg',
-      image2: './images/slider-img2.jpg',
-      displacementImage: './images/displacement/1.jpg',
-    });
-  }, []);
+  // useEffect(() => {
+  //   const x = new hoverEffect({
+  //     parent: img.current,
+  //     intensity: 0.3,
+  //     image1: './images/slider-img1.jpg',
+  //     image2: './images/slider-img2.jpg',
+  //     displacementImage: './images/displacement/1.jpg',
+  //   });
+  // }, []);
+
   return (
     <Container>
       <Header />
-      <Image src="/images/slider-img1.jpg" width={1920} height={773} alt="xx" />
-      <Image src="/images/slider-img2.jpg" width={1920} height={773} alt="xx" />
-      <div ref={img} className="landing-page-slider"></div>
+      <div className="temp-img-container">
+        <Image src="/images/slider-img2.jpg" width={1920} height={773} alt="xx" />
+      </div>
+      <div className="spacer"></div>
+      {/* <div ref={img} className="landing-page-slider"></div> */}
     </Container>
   );
 };
