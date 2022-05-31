dc7cffe76c2163b7f7ccecdb1c1dffea9ca90737
batu <batuhan.topcu@appcent.mobi>
2022-05-31 15:02:26 +0300

    category page no result

--- frontend/pages/kategoriler/[slug].tsx
+++ frontend/pages/kategoriler/[slug].tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { getCategories, getProducts } from '../../axios/getters';
 import { CategoryFlat } from '../../types';
 import { Container, ProductGrid } from '../../components';
+import { useRouter } from 'next/router';
 
 export async function getStaticPaths() {
   const products = await getProducts();
@@ -29,10 +30,20 @@ export async function getStaticProps({ params }: { params: { slug: string } }) {
 }
 
 export default function SingleProduct({ category }: { category: CategoryFlat }) {
+  const router = useRouter();
+
   return (
     <Container>
       {category.products.length > 0 && (
         <ProductGrid products={category.products} title={category.name} />
+      )}
+      {category.products.length === 0 && (
+        <div className="search-result__container">
+          <h4>
+            &quot;<span>{category.name}</span>&quot; kategorisinde henüz ürün yok.
+          </h4>
+          <button onClick={() => router.push('/')}>Ana Sayfaya Dön</button>
+        </div>
       )}
     </Container>
   );
