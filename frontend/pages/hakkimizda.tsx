072ffaa896e7a80ede37925d57267939a7dcf63b
Can Berk Sanal <48856495+cbsanal@users.noreply.github.com>
2022-06-03 21:47:06 +0300

    contact and abour pages refactored

--- frontend/pages/hakkimizda.tsx
+++ frontend/pages/hakkimizda.tsx
@@ -1,7 +1,9 @@
 import { Container, SimpleLayout } from '../components';
 import Image from 'next/image';
+import { useState } from 'react';
 
 const About = () => {
+  const [activeImg, setActiveImg] = useState('/images/dukkan-1.jfif');
   return (
     <Container>
       <SimpleLayout
@@ -10,7 +12,33 @@ const About = () => {
             been the industrys standard dummy text ever since the 1500s, when an unknown printer took a
             galley of type and scrambled it to make a type specimen book. It has survived not only five
             centuries, but also the leap into electronic">
-        <Image src="/images/shopImg.png" width={827} height={530} alt="Dükkan Resmi" />
+        <div className="about-img-gallery-container">
+          <Image
+            className="about-img-gallery-main-img"
+            src={activeImg}
+            width={827}
+            height={530}
+            alt="Dükkan Resmi"
+          />
+          <div className="about-img-gallery-options">
+            {[
+              '/images/dukkan-1.jfif',
+              '/images/dukkan-2.jpg',
+              '/images/dukkan-3.jpg',
+              '/images/dukkan-4.jpg',
+            ].map((imgSrc, i) => (
+              <Image
+                key={i}
+                className={activeImg === imgSrc ? 'active' : ''}
+                onClick={() => setActiveImg(imgSrc)}
+                src={imgSrc}
+                width={140}
+                height={120}
+                alt="Dükkan Resmi"
+              />
+            ))}
+          </div>
+        </div>
       </SimpleLayout>
     </Container>
   );
